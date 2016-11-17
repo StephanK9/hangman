4abfd7eecf5308f7682727166f0ddfe09c4555d4
Bryant Wang and Stephan Khoury <bryant+stephan@>
2016-11-17 10:26:06 -0800

    Add index.cshtml, add form and hangman to index.cshtml.

--- Objects/Game.cs
+++ Objects/Game.cs
@@ -8,13 +8,12 @@ namespace Hangman.Objects
         private int _guess;
         private bool _gameOver;
         private string _setWord;
-        private List<string> _wordLists = new List<string> {};
+        private List<string> _wordLists = new List<string> { "Venice", "Lyon", "Rio de Janeiro", "Las Vegas", "New York", "Los Angeles", "Columbus", "Cleveland", "Dallas", "Fort Worth", "New Orleans", "Portland", "San Francisco", "Tokyo", "Kyoto", "Fukushima", "Yanagawa", "Onomichi", "Sapporo", "London", "Barcelona", "Rome", "Vancouver", "Moscow", "Beijing", "Ankara", "Florence", "Houston", "Miami", "Munich", "Berlin", "Singapore", "Dubai", "Cairo", "Salem", "San Diego", "Atlanta", "Chicago", "Seattle", "New Delhi", "Ho Chi Minh City", "Macao", "Taipei", "Johannesburg", "Montreal", "Fort Lauderdale", "Tampa", "Lubbock", "El Paso", "Irvine", "Santa Fe", "Anaheim", "Lake Forest", "St Petersburg", "Krasnodar", "Sochi", "Diamond Bar", "Istanbul", "Rancho Santa Margarita", "Orange", "Riverside", "Newport Beach", "Beverly Hills", "Glendale", "Burbank", "Oakland", "Redding", "Bakersfield", "Reno", "Carson City", "Olympia", "Nice", "Paris", "Madrid", "Amsterdam", "Oslo", "Stockholm", "Reykjavik", "Hammerfest", "Tromso" };
 
         public Game()
         {
             _guess = 0;
             _gameOver = false;
-            _wordLists = { "Venice", "Lyon", "Rio de Janeiro", "Las Vegas", "New York", "Los Angeles", "Columbus", "Cleveland", "Dallas", "Fort Worth", "New Orleans", "Portland", "San Francisco", "Tokyo", "Kyoto", "Fukushima", "Yanagawa", "Onomichi", "Sapporo", "London", "Barcelona", "Rome", "Vancouver", "Moscow", "Beijing", "Ankara", "Florence", "Houston", "Miami", "Munich", "Berlin", "Singapore", "Dubai", "Cairo", "Salem", "San Diego", "Atlanta", "Chicago", "Seattle", "New Delhi", "Ho Chi Minh City", "Macao", "Taipei", "Johannesburg", "Montreal", "Fort Lauderdale", "Tampa", "Lubbock", "El Paso", "Irvine", "Santa Fe", "Anaheim", "Lake Forest", "St Petersburg", "Krasnodar", "Sochi", "Diamond Bar", "Istanbul", "Rancho Santa Margarita", "Orange", "Riverside", "Newport Beach", "Beverly Hills", "Glendale", "Burbank", "Oakland", "Redding", "Bakersfield", "Reno", "Carson City", "Olympia", "Nice", "Paris", "Madrid", "Amsterdam", "Oslo", "Stockholm", "Reykjavik", "Hammerfest", "Tromso" }
         }
 
         public void AddWord(string word)
@@ -43,7 +42,7 @@ namespace Hangman.Objects
         public void ChooseWord()
         {
             Random rnd = new Random();
-            int whichWord = rnd.Next(0, _wordlists.Count);
+            int whichWord = rnd.Next(0, _wordLists.Count);
             Console.WriteLine("random number: " + whichWord);
             _setWord = _wordLists[whichWord];
             Console.WriteLine("set word: " + _setWord);
